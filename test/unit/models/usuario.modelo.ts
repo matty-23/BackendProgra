db58351b8a46b88e626b2a4b5c316cebaa72d427
Matty-23 <matu.naser@gmail.com>
2026-08-10 19:04:45 -0300

    UsuarioController ha fallado con exito

--- test/unit/models/usuario.modelo.ts
+++ test/unit/models/usuario.modelo.ts
@@ -1,4 +1,5 @@
 import { Usuario } from "../../../src/Models/Usuario";
+import { UsuarioDto } from "../../../src/DTO/UsuarioDTO"
 export const modelosUsuarios = {
 
     documentoUsuario1: new Usuario(
@@ -9,7 +10,7 @@ export const modelosUsuarios = {
         'MairaTr',
         '12345'
     ),
-    
+
     documentoUsuario2: new Usuario(
         'usuario-2',
         'Maira',
@@ -20,3 +21,12 @@ export const modelosUsuarios = {
     ),
 
 }
+
+export const modelosUsuariosDto = {
+        id: 'usuario-2',
+        nombre: 'Maira',
+        apellido: 'Troillan',
+        email: 'maira.troillan@example.com',
+        username: 'MairaTr',
+        password: '12345'
+    } as UsuarioDto
--- test/unit/usuario/usuarioController.spec.ts
+++ test/unit/usuario/usuarioController.spec.ts
@@ -1,26 +1,28 @@
 import { beforeEach, describe, it, expect, jest } from '@jest/globals';
 import { UsuarioController } from "../../../src/Controller/UsuarioController"
 import { usuarioServiceMock } from "./mocks/usuarioService.mock"
-import { IUsuarioService } from '../../../src/Interfaces/IUsuarioService';
-import { UsuarioService } from '../../../src/Service/UsuarioService';
-import { modelosUsuarios } from "../models/usuario.modelo"
+//import { IUsuarioService } from '../../../src/Interfaces/IUsuarioService';
+//import { UsuarioService } from '../../../src/Service/UsuarioService';
+import * as model from "../models/usuario.modelo"
 
-export class usuarioControllerTest {
 
-    async GetTest() {
-        describe("UsuarioController", () => {
-            let controller: UsuarioController;
 
-            beforeEach(() => {
-                jest.clearAllMocks();
-                controller = new UsuarioController(usuarioServiceMock as any);
-            });
+describe("UsuarioController", () => {
+    let controller: UsuarioController;
 
-            it("Get by Id- Return 200", async() =>{
-                usuarioServiceMock.getUsuarioById.mockResolvedValue(modelosUsuarios.documentoUsuario1);
-                
-                const resultado = await controller.getById({id : modelosUsuarios.documentoUsuario1.getId()});
-            });
-        });
-    }
-}
+    beforeEach(() => {
+        jest.clearAllMocks();
+        controller = new UsuarioController(usuarioServiceMock as any);
+    });
+
+    it("Get by Id- Return 200", async () => {
+        usuarioServiceMock.getUsuarioById.mockResolvedValue(model.modelosUsuarios.documentoUsuario2);
+
+        const resultado = await controller.getById({ id: model.modelosUsuarios.documentoUsuario2.getId() });
+        expect(resultado).toEqual(model.modelosUsuariosDto);
+        expect(usuarioServiceMock.getUsuarioById).toHaveBeenCalledWith(model.modelosUsuarios.documentoUsuario2.getId());
+    });
+
+
+});
+
