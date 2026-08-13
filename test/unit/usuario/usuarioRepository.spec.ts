c5065032a156032174fb45a18a7bb2ab8cfc1624
matty-23 <matu.naser@gmail.com>
2026-08-13 23:04:31 +0000

    Se empezaron con los test del repository

---
+++ test/unit/usuario/usuarioRepository.spec.ts
@@ -0,0 +1,29 @@
+import { beforeEach, describe, it, expect, jest, } from '@jest/globals';
+import * as model from "../models/usuario.modelo";
+import { Types,} from 'mongoose';
+import { UsuarioModel } from '../../../src/Database/Schemes/UsuarioScheme';
+import { UsuarioRepository } from '../../../src/Database/Context/UsuarioRepository';
+import { transactionContext } from '../../../src/Database/TransactionContext';
+
+describe("Test usuarioRepository", ()=>{
+    let repository: UsuarioRepository
+    beforeEach(()=>{
+         jest.clearAllMocks();
+         repository=new UsuarioRepository();
+    });
+
+    it("Crear Usuario - Devuelve ID",async ()=>{
+        const id =new Types.ObjectId()
+        const saveMock = jest.spyOn(UsuarioModel.prototype, 'save').mockResolvedValue({_id:  id,} as any);
+        jest.spyOn(transactionContext, 'getStore').mockReturnValue(undefined);
+        const resultado = await repository.crearUsuario(model.modelosUsuarios.documentoUsuario1.getNombre(),
+        model.modelosUsuarios.documentoUsuario1.getApellido(),
+        model.modelosUsuarios.documentoUsuario1.getEmail(),
+        model.modelosUsuarios.documentoUsuario1.getUsername(),
+        model.modelosUsuarios.documentoUsuario1.getPassword(),
+    );
+        expect(resultado).toEqual(id);
+        expect(saveMock).toHaveBeenCalledWith({});
+        expect(saveMock.mock.instances[0]).toBeDefined();
+        });
+});
